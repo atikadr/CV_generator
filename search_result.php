7343cd5473e523e8617b191cf5fc71168381c31d
atikadr <atika.d.r@gmail.com>
2013-12-14 19:56:32 +0800

    Edited search_result to only show search results

--- search_result.php
+++ search_result.php
@@ -38,19 +38,35 @@
 	else
 		if(!empty($codename)){
 			$result = mysqli_query($con,"SELECT * FROM project WHERE UPPER(code_name) LIKE UPPER('%$codename%')");
+			echo "<h3>Daftar proyek berdasarkan search: </h3>";
 
 			while ($row = mysqli_fetch_array($result)){
-				echo $row['project_name'], " ";
-				echo $row['project_client'], " ";
-				echo $row['code_name'];
+				$chosenproject = $row['project_name'];
+				$pieces = explode(" ", $chosenproject);
+				$newstring = NULL;
+
+				for($i = 0 ; $i < count($pieces) ; $i++){
+					$newstring = $newstring . $pieces[$i];
+					if ($i != count($pieces) - 1) {$newstring = $newstring . "+";}
+				}
+				echo "<a href=edit_project.php?chosenproject=$newstring>$chosenproject<br />";
 			}
 		}
 		else
 			if(!empty($employeename)){
 				$result = mysqli_query($con,"SELECT * FROM employee WHERE UPPER(employee_name) LIKE UPPER('%$employeename%')");
+				echo "<h3>Daftar anggota berdasarkan search: <h3>";
 
 				while($row = mysqli_fetch_array($result)){
-					echo $row['employee_name'];
+					$chosenemployee = $row['employee_name'];
+					$pieces = explode(" ", $chosenemployee);
+					$newstring = NULL;
+
+					for($i = 0 ; $i < count($pieces) ; $i++){
+						$newstring = $newstring . $pieces[$i];
+						if ($i != count($pieces) - 1) {$newstring = $newstring . "+";}
+					}
+					echo "<a href=edit_employee.php?chosenemployee=$newstring>$chosenemployee<br />";
 				}
 			}
 
