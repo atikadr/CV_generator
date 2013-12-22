5ea67deaa07bba8808dd0ba77e99fec455683632
atikadr <atika.d.r@gmail.com>
2013-12-22 12:15:53 +0800

    Edited new project for new fields

--- new_project.php
+++ new_project.php
@@ -9,18 +9,18 @@
 <h2>Proyek baru</h2>
 <body>
 
-<form method="post" action="new_project_success.php">
+<form method="get" action="new_project_success.php">
 	<table border=1 style="border-collapse:collapse">
 		<tr><td>Nama Proyek:</td><td><input type ="text" name="projectname" id="projectname" style='width:180px'></td></tr>
-		<tr><td>Client:</td><td><input type ="text" name="projectclient" id="projectclient" style='width:180px'></td></tr>
-		<tr><td>Lokasi:</td><td><input type="text" name="location" id="location" style='width:180px'></td></tr>
-		<tr><td>Nomor SPK:</td><td><input type="text" name="nomorSPK" id="nomorSPK" style='width:180px'></td></tr>
 		<tr><td>Code name:</td><td><input type="text" name="codename" id="codename" style='width:180px'></td></tr>
-		<tr><td>Mulai kerja:</td><td><input type="text" name="starttime" id="starttime" style='width:180px'></td></tr>
-		<tr><td>Selesai kerja:</td><td><input type="text" name="endtime" id="endtime" style='width:180px'></td></tr>
-		<tr><td>Nomor BAPP:</td><td><input type="text" name="nomorBAPP" id="nomorBAPP" style='width:180px'></td></tr>
-		<tr><td>Tanggal BAPP:</td><td><input type="text" name="tanggalBAPP" id="tanggalBAPP" style='width:180px'></td></tr>
-		<tr><td style='width:200px'>Keterangan tambahan:</td><td style='width:200px'><input type="text" name="description" id="description" style='width:180px'></td></tr>
+		<tr><td>Lokasi:</td><td><input type="text" name="location" id="location" style='width:180px'></td></tr>
+		<tr><td>Client:</td><td><input type ="text" name="projectclient" id="projectclient" style='width:180px'></td></tr>
+		<tr><td>Alamat client:</td><td><input type="text" name="clientaddress" id="clientaddress" style='width:180px'></td></tr>
+		<tr><td>Nomor SPK:</td><td><input type="text" name="nomorSPK" id="nomorSPK" style='width:180px'></td></tr>
+		<tr><td>Mulai kerja (YYYY/MM/DD):</td><td><input type="text" name="startdate" id="startdate" style='width:180px'></td></tr>
+		<tr><td>Nilai:</td><td>Rp <input type="text" name="nilai" id="nilai" style='width:180px'></td></tr>
+		<tr><td>Selesai kerja (menurut kontrak) (YYYY/MM/DD):</td><td><input type="text" name="enddate" id="enddate" style='width:180px'></td></tr>
+		<tr><td style='width:200px'>Keterangan/deskripsi:</td><td style='width:200px'><input type="text" name="description" id="description" style='width:180px'></td></tr>
 	</table>
 	<br>
 	<input value="Submit" type="submit">
--- new_project_success.php
+++ new_project_success.php
@@ -4,28 +4,29 @@
 	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
+<a href=index.php>Home</a> > Proyek baru
+<br>
+
 <body>
 
 <?php
 
 	$con = mysqli_connect("localhost","root","bubumint","hr_dian");
-	$projectname = $_POST["projectname"];
-	$projectclient = $_POST["projectclient"];
-	$codename=$_POST["codename"];
-	$nomorSPK=$_POST["nomorSPK"];
-	$location=$_POST["location"];
-	$starttime=$_POST["starttime"];
-	$endtime=$_POST["endtime"];
-	$description=$_POST["description"];
-	$nomorBAPP=$_POST["nomorBAPP"];
-	$tanggalBAPP=$_POST["tanggalBAPP"];
+	$projectname = urldecode($_GET["projectname"]);
+	$projectclient = urldecode($_GET["projectclient"]);
+	$clientaddress=urldecode($_GET['clientaddress']);
+	$codename=urldecode($_GET["codename"]);
+	$nomorSPK=urldecode($_GET["nomorSPK"]);
+	$location=urldecode($_GET["location"]);
+	$startdate=urldecode($_GET["startdate"]);
+	$enddate=urldecode($_GET["enddate"]);
+	$description=urldecode($_GET["description"]);
+	$nilai=urldecode($_GET['nilai']);
 
-	mysqli_query($con,"INSERT INTO Project VALUES('$projectname','$codename','$nomorSPK','$location','$projectclient','$starttime','$endtime','$description','$nomorBAPP','$tanggalBAPP')");
-	$employees = mysqli_query($con,"SELECT * FROM Employee");
-	$position = mysqli_query($con,"SELECT * FROM Employee_position_type");
+	mysqli_query($con,"INSERT INTO Project VALUES('$projectname','$codename','$location','$projectclient','$clientaddress','$nomorSPK','$startdate','$enddate','$description','$nilai')");
 ?>
 
-Proyek sudah disimpan. DO NOT CLICK BACK. Add employee di <a href=add_employee_to_project.php>sini</a>.<br>
+Proyek sudah disimpan. DO NOT CLICK BACK/REFRESH. Add anggota di <a href=add_employee_to_project.php>sini</a>.<br>
 <a href=index.php>Home</a>
 
 
